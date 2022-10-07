da181edc47f98f122cb038063438db562dbb5d4f
Adilet Yerkin <adilet.yerkin@gmail.com>
2022-10-07 14:58:47 +0600

    Task2 json

--- Task 2/chart.js
+++ Task 2/chart.js
@@ -3,9 +3,11 @@ async function buildPlot() {
     const data = await d3.json("./my_weather_data.json");
     //console.table(data);
     const dateParser = d3.timeParse("%Y-%m-%d");
-    const yAccessor = (d) => d.temperatureMin;
-    const yAccessorHigh = (d) => d.temperatureHigh;
-    const xAccessor = (d) => dateParser(d.date);
+    // const yAccessor = (d) => d.temperatureMin;
+    // const yAccessorHigh = (d) => d.temperatureHigh;
+    // const xAccessor = (d) => dateParser(d.date);
+    const xAccessor = d => d[temp];
+    const yAccessor = d => d.length;
     // Функции для инкапсуляции доступа к колонкам набора данных
 
     var dimension = {
@@ -27,45 +29,74 @@ async function buildPlot() {
     svg.attr("height",dimension.height);
     svg.attr("width",dimension.width);
     const bounded = svg.append("g");
-    bounded.style("transform",`translate(${dimension.margin.left}px, ${dimension.margin.top})`);
+
+    const bounds = wrapper.append("g")
+        .style("translate",`translate(${dimensions.margin.left}px,${dimensions.margin.top}px)`);
+
+    const xScaler = d3.scaleLinear()
+        .domain(d3.extent(dataset,humidityAccessor))
+        .range([0,dimensions.boundedWidth])
+        .nice()
+
+    const binsGen = d3.bin()
+        .domain(xScaler.domain())
+        .value(humidityAccessor)
+        .thresholds(12);
+
+    const bins = binsGen(dataset);
+    console.log(bins);
 
     const yScaler = d3.scaleLinear()
-        .domain(d3.extent(data,yAccessor))
-        .range([dimension.boundedHeight,0]);
+        .domain([0, d3.max(bins, yAccessor)])
+        .range([dimensions.boundedHeight,0])
 
-    const xScaler = d3.scaleTime()
-        .domain(d3.extent(data,xAccessor))
-        .range([0,dimension.boundedWidth]);
+    const binGroup = bounds.append("g");
+    const binGroups = binGroup.selectAll("g")
+        .data(bins)
+        .enter()
+        .append("g");
 
 
-    var lineGenerator = d3.line()
-        .x(d => xScaler(xAccessor(d)))
-        .y(d => yScaler(yAccessor(d)));
+    const barPadding = 1
+    const barRect = binGroups.append("rect")
+        .attr("x", d => xScaler(d.x0) + barPadding/2)
+        .attr("y", d => yScaler(yAccessor(d)))
+        .attr("width", d => d3.max([0, xScaler(d.x1) - xScaler(d.x0) - barPadding]))
+        .attr("height", d => dimensions.boundedHeight - yScaler(yAccessor(d)))
+        .attr("fill", "#AAAAEE");
 
-    var lineGeneratorHigh = d3.line()
-        .x(d => xScaler(xAccessor(d)))
-        .y(d => yScalerHigh(yAccessorHigh(d)));
+    const mean = d3.mean(dataset,humidityAccessor);
+    console.log(mean);
+    const meanLine = bounds.append("line")
+        .attr("x1", xScaler(mean))
+        .attr("x2", xScaler(mean))
+        .attr("y1", -15)
+        .attr("y2", dimensions.boundedHeight)
+        .attr("stroke","black")
+        .attr("stroke-dasharray","2px 4px");
 
-    bounded.append("path")
-        .attr("d",lineGenerator(data))
-        .attr("fill","none")
-        .attr("stroke","blue")
+    const meanLabel = bounds.append("text")
+        .attr("x",xScaler(mean))
+        .attr("y",10)
+        .text("Mean")
+        .attr("fill","maroon")
+        .attr("font-size","12px")
+        .attr("text-anchor","middle");
 
-    bounded.append("path")
-        .attr("d",lineGeneratorHigh(data))
-        .attr("fill","none")
-        .attr("stroke","red")
+    const xAxisGen = d3.axisBottom()
+        .scale(xScaler);
+    const xAxis = bounds.append("g")
+        .call(xAxisGen)
+        .style("transform",`translateY(${dimensions.boundedHeight}px)`);
 
-    const yAxisGenerator = d3.axisLeft(yScaler);
-    bounded.append("g")
-        .call(yAxisGenerator)
-        .style("transform", `translate(${dimension.boundedWidth + 20}px)`) ;
-
-    const xAxisGenerator = d3.axisBottom(xScaler);
-    bounded.append("g")
-        .call(xAxisGenerator.tickFormat(d3.timeFormat("%m-%d")))
-        .style("transform", `translateY(${dimension.boundedHeight}px)`);
-
+    const barText = binGroups.filter(yAccessor)
+        .append("text")
+        .attr("x", d => xScaler(d.x0) + (xScaler(d.x1)-xScaler(d.x0))/2)
+        .attr("y", d => yScaler(yAccessor(d)) - 5)
+        .text(yAccessor)
+        .attr("fill","darkgrey")
+        .attr("font-size","12px")
+        .attr("text-anchor","middle");
 
 
 }
